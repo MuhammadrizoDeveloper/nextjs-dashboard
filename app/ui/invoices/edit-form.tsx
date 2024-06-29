7df2c0dd23bd1f8ed9aa8b16771856d6158cc6b7
Muhammadrizo <muhammadrizo.dev@gmail.com>
2024-06-29 08:59:11 +0500

    login

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -10,6 +10,7 @@ import {
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 import { updateInvoice } from '@/app/lib/actions';
+import { useActionState } from 'react';
 
 export default function EditInvoiceForm({
   invoice,
@@ -18,7 +19,9 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  // const initialState = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  // const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
 
   return (
     <form action={updateInvoiceWithId}>
